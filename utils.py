f9a0b3007e20c2aaed363d11994dd8603e8309d0
AyonRRahman <raqibur.ayon@gmail.com>
2024-01-09 18:00:50 +0100

    saving navigation data with x and y values

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
 import cv2
+import pandas as pd 
+import utm
+
 import os 
 
 def show_image_sequence(image_folder: str):
@@ -28,6 +31,58 @@ def show_image_sequence(image_folder: str):
     
     cv2.destroyAllWindows()
 
+
+
+def convert_latlon_utm(dataset_folder: str):
+    '''
+    this function takes a folder containing navigation.txt file and converts the lat,lon
+    system to utm projection system and saves the result as a csv file. 
+
+    Args:
+        dataset_folder (str): Path to the folder containing navigation.txt file.
+
+    '''
+    if not isinstance(dataset_folder, str):
+        raise ValueError("Please provide a string as the image_folder argument.")
+    
+    
+    if not os.path.exists(dataset_folder):
+        print(f"The folder {dataset_folder} does not exist.")
+        return 
+    
+    if not os.path.exists(os.path.join(dataset_folder, 'navigation.txt')):
+        print(f"There is no navigation.txt file in the folder {dataset_folder}.")
+        return 
+    
+
+    #read the navigation.txt
+    df = pd.read_csv(os.path.join(dataset_folder, 'navigation.txt'), delimiter=' ',index_col='name')
+
+    east = []
+    north = []
+
+    #convert latlon to utm 
+    for index, rows in df.iterrows():
+        lat = rows['lat']
+        lon = rows['lon']
+        easting, northing, zone_number,_ = utm.from_latlon(lat, lon)
+        east.append(easting)
+        north.append(northing)
+    
+    df['north'] = north
+    df['east'] = east
+
+    #save to csv
+    df.to_csv(os.path.join(dataset_folder,'navigation.csv'))
+
+
+
 if __name__=='__main__':
-    image_folder = '/home/ayon/thesis/data/Eiffel-Tower/2018/images'
-    show_image_sequence(image_folder)
+    dataset_folder = '/home/ayon/thesis/data/Eiffel-Tower/'
+    for year in os.listdir(dataset_folder):
+        print(year)
+        dataset = os.path.join(dataset_folder,year)
+        convert_latlon_utm(dataset)
+        
+
+    
