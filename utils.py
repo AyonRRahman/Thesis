7cf7d312dc5e53c06c9c7084c3e5a43bb5a96652
AyonRRahman <raqibur.ayon@gmail.com>
2024-01-10 14:52:50 +0100

    Added Processing for images.txt files

--- utils.py
+++ utils.py
@@ -2,6 +2,8 @@ import cv2
 import pandas as pd 
 import numpy as np
 import utm
+from scipy.spatial.transform import Rotation as R
+
 
 import os 
 
@@ -34,8 +36,9 @@ def show_image_sequence(image_folder: str):
 
 
 
-def convert_latlon_utm(dataset_folder: str):
+def Eiffel_convert_latlon_utm(dataset_folder: str):
     '''
+    For Eiffel Tower dataset.
     this function takes a folder containing navigation.txt file and converts the lat,lon
     system to utm projection system and saves the result as a csv file. 
 
@@ -84,12 +87,77 @@ def convert_latlon_utm(dataset_folder: str):
     df.to_csv(os.path.join(dataset_folder,'navigation.csv'))
 
 
+def Eiffel_save_navigation_data(file_path: str) -> pd.DataFrame:
+
+    '''
+    This function takes images.txt file from the Eiffel tower dataset and saves a csv file
+    containing the trajectory along wih euler angles and quaternions. 
+    Args:
+        file_path (str): Path to the images.txt file.
+
+    Returns:
+        df (pd.DataFrame): Processed dataframe
+
+    '''
+
+    if os.path.exists(file_path):
+
+        #list to store the data
+        data = []
+
+        #open and process the txt file
+        with open(file_path, 'r') as f:
+            for index, line in enumerate(f):
+                if index<4:
+                    continue
+            
+                if index%2!=0:
+                    continue
+                
+                line_content = line.split(' ')
+                
+                image_name = line_content[-1]
+                QW = line_content[1]
+                QX = line_content[2]
+                QY = line_content[3]
+                QZ = line_content[4]
+                TX = line_content[5]
+                TY = line_content[6]
+                TZ = line_content[7]
+                Camera_id = line_content[8]
+
+
+                # get euler angles from quaternions
+                r = R.from_quat([QW, QX, QY, QZ])
+                Rz, Ry, Rx = r.as_euler('zyx', degrees=True)
+                
+                #append the data in the list
+                data.append([image_name[:-1], QW, QX, QY, QZ, TX, TY, TZ, Rx, Ry, Rz])
+
+        # make the dataframe and process it for saving
+        df = pd.DataFrame(data, columns=['image_name', 'QW', 'QX', 'QY', 'QZ', 'TX', 'TY', 'TZ', 'Rx', 'Ry', 'Rz'])
+        df = df.sort_values(by='image_name')
+        df = df.set_index('image_name')
+        df = df.astype(float)
+        return df
+    
+    else:
+        print(f"The file '{file_path}' does not exist.")
+        return None
+
+
 if __name__=='__main__':
     dataset_folder = '/home/ayon/thesis/data/Eiffel-Tower/'
     for year in os.listdir(dataset_folder):
         print(year)
-        dataset = os.path.join(dataset_folder,year)
-        convert_latlon_utm(dataset)
+        file_path = os.path.join(dataset_folder,year+'/sfm/images.txt')
         
+        
+        df = Eiffel_save_navigation_data(file_path)
 
+        if isinstance(df, pd.DataFrame):
+            df.to_csv(os.path.join(dataset_folder,year+'/sfm/position_and_orientation.csv'))
+        
+        else:
+            print('Function did not return Dataframe')
     
