7e6e3a87e7674375bf8f2d358fcf3ff9b987f0a6
AyonRRahman <raqibur.ayon@gmail.com>
2024-01-29 16:20:22 +0100

    tested_datasetup_in cluster

---
+++ data/datasetup_Eiffel_Tower_slurm.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --time=2-00:01:00
+#SBATCH --job-name=datasetup_Eiffel
+#SBATCH --output=datasetup_Eiffel.out
+#SBATCH --error=datasetup_Eiffel.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=raqibur.ayon@gmail.com
+
+module load colmap
+module load cuda/11.0 
+
+echo '---------------'
+which python
+conda init
+conda activate thesis
+which python
+echo '---------------'
+
+echo $HOSTNAME
+
+./datasetup_Eiffel_Tower.sh
+
+echo $HOSTNAME
